544ed86e3664dcd2cff0041198e9fb6eb9ebb5c7
127136013+byschoo@users.noreply.github.com <byschoo@outlook.com>
2024-10-17 23:08:00 +0200

    REFRESH ARRAYLIST E ITERATOR

--- src/OrdenarPersonaPorAltura.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class OrdenarPersonaPorAltura implements Comparator<Persona> {
-
-    @Override
-    public int compare(Persona o1, Persona o2) {
-        return o1.getAltura() - o2.getAltura(); //Devuelve un entero positivo si la altura de o1 es mayor que la de o2
-    }
-}
--- src/Persona.java
+++ src/Persona.java
@@ -1,24 +1,22 @@
-/* Ejemplo Interface Comparator vs Comparable aprenderaprogramar.com */
+/* Ejemplo Interfaz List aprenderaprogramar.com */
 
-public class Persona implements Comparable<Persona> {
+public class Persona{
+
     private int idPersona;
     private String nombre;
-    private int altura;
+    private int altura; 
 
-    public Persona (int idPersona, String nombre, int altura) {
+    public Persona(int idPersona, String nombre, int altura) {
+
         this.idPersona = idPersona;
         this.nombre = nombre;
-        this.altura = altura;}
+        this.altura = altura;
+    }
+
+    public int getAltura() { return altura; } //Omitimos otros métodos get y set para simplificar 
 
     @Override
     public String toString() {
-        return "\nPersona-> ID: "+idPersona+" Nombre: "+nombre+" Altura: "+altura;}
-
-    @Override
-    public int compareTo(Persona o) {
-        return this.nombre.compareTo(o.nombre);}
-
-    public int getIdPersona() {return idPersona;}
-    public String getNombre() {return nombre;}
-    public int getAltura() {return altura;}
+        return "Persona-> ID: "+idPersona+" Nombre: "+nombre+" Altura: "+altura+"\n";
+    }
 }
