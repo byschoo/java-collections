ee61d297ef479f5c19b7fd94eff24d4c407aed9e
127136013+byschoo@users.noreply.github.com <byschoo@outlook.com>
2024-10-18 03:24:51 +0200

    REFRESH MAP - TREEMAP

--- src/App.java
+++ src/App.java
@@ -1,11 +1,11 @@
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        Map<String, Double> inventarioHash = new LinkedHashMap<>(); //No hay orden.
+        Map<String, Double> inventarioHash = new TreeMap<>(); //No hay orden.
 
         System.out.println("Bienvenido a la Frutería del Abuelo");
 
