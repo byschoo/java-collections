769ff2dce32ce44e717bd7f11a2beda8ad51bded
127136013+byschoo@users.noreply.github.com <byschoo@outlook.com>
2024-10-18 03:18:43 +0200

    REFRESH MAP - HASHMAP

--- src/App.java
+++ src/App.java
@@ -1,58 +1,35 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        Map<String, Double> inventarioHash = new HashMap<>(); //No hay orden.
 
-        List<String> superheroes = new ArrayList<>(); //Ordena de acuerdo a como fue insertado, van por índice y se pueden duplicar
+        System.out.println("Bienvenido a la Frutería del Abuelo");
 
-        superheroes.add("Superman".toUpperCase());
-        superheroes.add("Wonderwoman".toUpperCase());
-        superheroes.add("Batman".toUpperCase());
-        superheroes.add("Robbin".toUpperCase());
-        superheroes.add("Aquaman".toUpperCase());
-        superheroes.add("Flash".toUpperCase());
-        superheroes.add("Robbin".toUpperCase());
-        superheroes.add("Robbin".toUpperCase());
-        superheroes.add("Robbin".toUpperCase());
+        inventarioHash.put("Banana", 1.45);
+        inventarioHash.put("Platano", 1.85);
+        inventarioHash.put("Naranja", 1.75);
+        inventarioHash.put("Mandarina", 1.25);
+        inventarioHash.put("Fresa", 2.45);
+        inventarioHash.put("Mango", 3.15);
 
-        System.out.println("Hay "+ superheroes.size() +" superhéroes que han llegado a la fiesta: ");
-
-        for (String superheroe : superheroes) {
-            System.out.println(superheroe);
+        System.out.println("El inventario es el siguiente: ");
+        
+        for (String fruta : inventarioHash.keySet()) {
+            System.out.println(fruta + ": €" + inventarioHash.get(fruta));
         }
-        
-        System.out.print("\nHay superhéroes que se van de la fiesta (Y/N): ");
-        String centinela = scanner.nextLine().toUpperCase();
-        
-        while (centinela.equals("Y")) {
-            System.out.println("Quién se va: ");
-            String seRetiran = scanner.nextLine().toUpperCase();
 
-                if (superheroes.contains(seRetiran)) {
-                    superheroes.remove(seRetiran.toUpperCase());
-                    System.out.print("Ya se fue " + seRetiran.toUpperCase() + " de la fiesta.\n");
-                }else {
-                    System.out.print(seRetiran + " no ha estado en la fiesta.\n");
-                }
+        System.out.print("\nQuieres buscar una fruta: ");
+        String chekFruta = scanner.nextLine();
 
-            System.out.print("\nHay otro superhéroe que se va de la fiesta (Y/N): ");
-            centinela = scanner.nextLine().toUpperCase();
+        if (inventarioHash.containsKey(chekFruta)) {
+            System.out.println("Pide, que te la tengo. Cuánto quieres?");
+        } else {
+            System.out.println("No la tenemos, nos llega mañana.");
         }
-        
-        System.out.println(superheroes.get(0) + " está ebrio, se quitó el traje y revela su verdadera identidad.");
-        superheroes.set(0, "Clark ken");
-
-        System.out.println("\nQuedan "+ superheroes.size() +" superhéroes aún en la fiesta: ");
-        
-        for (String superheroe : superheroes) {
-            System.out.println(superheroe.toUpperCase());
-        }
-        
-        System.out.println("\n"+ superheroes.get(3) + " está ebrio.\n");
-
 
         scanner.close();
     }
